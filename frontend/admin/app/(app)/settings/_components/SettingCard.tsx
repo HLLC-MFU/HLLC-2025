a610d45db2bb7e2a79a2d9f423c73d4bc586cdbf
poonyawat0511 <poonyawat0931@gmail.com>
2025-05-31 23:18:21 +0700

    feat(settings): implement system management with SystemCard and useSystem hook

---
+++ frontend/admin/app/(app)/settings/_components/SettingCard.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody, CardHeader } from "@heroui/react";
+
+
+export default function SettingCard() {
+    return (
+        <Card>
+            <CardHeader>
+                
+            </CardHeader>
+            <CardBody>
+
+            </CardBody>
+        </Card>
+    )
+}
