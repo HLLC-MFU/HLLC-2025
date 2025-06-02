12a7b5741979fd2c68a24eab830c0bdfb566c9e4
Wantana Suwannapho <6631503037@lamduan.mfu.ac.th>
2025-06-02 09:16:36 +0700

    feat: add components file

--- frontend/admin/app/(app)/evoucher/page.tsx
+++ frontend/admin/app/(app)/evoucher/page.tsx
@@ -19,6 +19,7 @@ import {
     SortDescriptor,
     Accordion,
     AccordionItem,
+    Chip,
 } from "@heroui/react";
 import { ChevronDownIcon, EllipsisVertical, PlusIcon, SearchIcon } from "lucide-react"
 
@@ -88,16 +89,16 @@ const mockupData: Evoucher[] = [
 import { Evoucher } from "@/types/evoucher";
 
 export const columns = [
-    { name: "SPONSOR", uid: "sponsor" },
-    { name: "ACRONYM", uid: "acronym", },
-    { name: "DETAIL", uid: "detail", },
-    { name: "DISCOUNT", uid: "discount", },
-    { name: "EXPIRATION", uid: "expiration", },
-    { name: "TYPE", uid: "type", },
+    { name: "SPONSOR", uid: "sponsor", sortable: true },
+    { name: "ACRONYM", uid: "acronym", sortable: true },
+    { name: "DETAIL", uid: "detail", sortable: true },
+    { name: "DISCOUNT", uid: "discount", sortable: true },
+    { name: "EXPIRATION", uid: "expiration", sortable: true },
+    { name: "TYPE", uid: "type", sortable: true },
     { name: "COVER", uid: "cover", },
-    // { name: "BANNER", uid: "banner", },
-    // { name: "THUMPNAIL", uid: "thumpnail", },
-    // { name: "LOGO", uid: "logo", },
+    { name: "BANNER", uid: "banner", },
+    { name: "THUMPNAIL", uid: "thumpnail", },
+    { name: "LOGO", uid: "logo", },
     { name: "ACTIONS", uid: "actions", },
 ];
 
@@ -111,7 +112,7 @@ const typeColorMap: Record<string, ChipProps["color"]> = {
     Individual: "warning",
 };
 
-const INITIAL_VISIBLE_COLUMNS = ["sponsor", "acronym", "detail", "discount", "expiration", "type", "cover", "actions"];
+const INITIAL_VISIBLE_COLUMNS = ["sponsor", "acronym", "detail", "discount", "expiration", "type", "actions"];
 
 export default function EvoucherPage() {
     const [evoucher, setEvoucher] = React.useState<Evoucher[]>([]);
@@ -140,7 +141,12 @@ export default function EvoucherPage() {
 
         if (hasSearchFilter) {
             filteredEvoucher = filteredEvoucher.filter((evoucher) =>
-                evoucher.sponsor.name.en.toLowerCase().includes(filterValue.toLowerCase()),
+                evoucher.sponsor.name.en.toLowerCase().includes(filterValue.toLowerCase()) ||
+                evoucher.type.name.toLowerCase().includes(filterValue.toLowerCase()) ||
+                evoucher.discount.toString().includes(filterValue.toLowerCase()) ||
+                evoucher.acronym.toLowerCase().includes(filterValue.toLowerCase()) ||
+                evoucher.detail.en.toLowerCase().includes(filterValue.toLowerCase()) ||
+                evoucher.expiration.toString().includes(filterValue.toLowerCase())
             );
         }
         if (typeFilter !== "all" && Array.from(typeFilter).length !== typeOptions.length) {
@@ -170,6 +176,12 @@ export default function EvoucherPage() {
                     month: "long",
                     day: "2-digit",
                 });
+            case "type":
+                return (
+                    <Chip color={typeColorMap[cellValue.name]} size="sm" variant="flat">
+                        {cellValue.name}
+                    </Chip>
+                )
             case "actions":
                 return (
                     <div className="relative flex justify-end items-center gap-2">
@@ -210,7 +222,7 @@ export default function EvoucherPage() {
                     <Input
                         isClearable
                         className="w-full sm:max-w-[44%]"
-                        placeholder="Search by name..."
+                        placeholder="Search evoucher"
                         startContent={<SearchIcon />}
                         value={filterValue}
                         onClear={() => onClear()}
@@ -260,7 +272,7 @@ export default function EvoucherPage() {
                             </DropdownMenu>
                         </Dropdown>
                         <Button color="primary" endContent={<PlusIcon />}>
-                            Add New
+                            Add Evoucher
                         </Button>
                     </div>
                 </div>
@@ -270,9 +282,9 @@ export default function EvoucherPage() {
         filterValue,
         typeFilter,
         visibleColumns,
+        typeOptions,
         onSearchChange,
         evoucher.length,
-        hasSearchFilter,
     ]);
 
     const bodyContent = React.useMemo(() => {
@@ -280,7 +292,7 @@ export default function EvoucherPage() {
             <Accordion
                 selectionMode="multiple"
                 itemClasses={{
-                    trigger: "px-4 my-1 data-[hover=true]:bg-default-100 rounded-lg"
+                    trigger: "px-4 mt-2 mb-1 data-[hover=true]:bg-default-100 rounded-lg"
                 }}
             >
                 {mockupData.map((evoucher) => {
@@ -299,6 +311,9 @@ export default function EvoucherPage() {
                                 hideHeader
                                 removeWrapper
                                 aria-label="Table data"
+                                // selectedKeys={selectedKeys}
+                                // selectionMode="multiple"
+                                // onSelectionChange={setSelectedKeys}
                             >
                                 <TableHeader columns={headerColumns}>
                                     {(column) => (
@@ -312,8 +327,8 @@ export default function EvoucherPage() {
                                 </TableHeader>
                                 <TableBody emptyContent={"No data found"} items={filteredItems.filter((data) => data.sponsor.name.en === evoucher.sponsor.name.en)}>
                                     {(item) => (
-                                        <TableRow key={item.sponsor.name.en}>
-                                            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                                        <TableRow key={item.acronym}>
+                                            {(columnKey) => <TableCell className="w-48">{renderCell(item, columnKey)}</TableCell>}
                                         </TableRow>
                                     )}
                                 </TableBody>
@@ -324,10 +339,12 @@ export default function EvoucherPage() {
             </Accordion>
         );
     }, [
-        onSearchChange,
-        onClear,
         filteredItems,
+        renderedSponsors,
+        mockupData,
+        visibleColumns,
         headerColumns,
+        topContent,
     ]);
 
     return (
@@ -336,39 +353,40 @@ export default function EvoucherPage() {
                 <div className="flex items-center justify-between mb-8">
                     <h1 className="text-3xl font-bold">Evoucher Management</h1>
                 </div>
-            </div>
 
-            <Table
-                isHeaderSticky
-                aria-label="Table header"
-                bottomContentPlacement="outside"
-                topContent={topContent}
-                topContentPlacement="outside"
-                // selectedKeys={selectedKeys}
-                // selectionMode="multiple"
-                // sortDescriptor={sortDescriptor}
-                // onSelectionChange={setSelectedKeys}
-                // onSortChange={setSortDescriptor}
-            >
-                <TableHeader columns={headerColumns}>
-                    {(column) => (
-                        <TableColumn
-                            key={column.uid}
-                            align={column.uid === "actions" ? "center" : "start"}
-                        // allowsSorting={column.sortable}
-                        >
-                            {column.name}
-                        </TableColumn>
-                    )}
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                        <TableCell colSpan={headerColumns.length} className="p-0">
-                            {bodyContent}
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+                <Table
+                    isHeaderSticky
+                    aria-label="Table header"
+                    bottomContentPlacement="outside"
+                    topContent={topContent}
+                    topContentPlacement="outside"
+                    // selectedKeys={selectedKeys}
+                    // selectionMode="multiple"
+                    // onSelectionChange={setSelectedKeys}
+                    sortDescriptor={sortDescriptor}
+                    onSortChange={setSortDescriptor}
+                >
+                    <TableHeader columns={headerColumns}>
+                        {(column) => (
+                            <TableColumn
+                                key={column.uid}
+                                align={column.uid === "actions" ? "center" : "start"}
+                                className="w-48"
+                                allowsSorting={column.sortable}
+                            >
+                                {column.name}
+                            </TableColumn>
+                        )}
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell colSpan={headerColumns.length} className="p-0">
+                                {bodyContent}
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     )
 }
