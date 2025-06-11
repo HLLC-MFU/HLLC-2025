726500aa7836a2f3a5022780df737adab2d1cb31
6731503119Waritpon <6731503119@lamduan.mfu.ac.th>
2025-06-11 21:43:41 +0700

    (feat) add user reference to LamduanFlowers schema; ensure user field is required and linked to User model

--- backend/app/src/module/lamduan-flowers/schema/lamduan-flowers.schema.ts
+++ backend/app/src/module/lamduan-flowers/schema/lamduan-flowers.schema.ts
@@ -1,3 +1,10 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import { Types } from "mongoose";
+
+@Schema({})
 export class LamduanFlowers {
+
+    @Prop({ required: true , type: Types.ObjectId , ref: 'User'})
+    user : Types.ObjectId;
 
 }
