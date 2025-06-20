82f14d47aceda2d6a64d77cf03b368d689e09695
WasanNachai <149573377+WasanNachai@users.noreply.github.com>
2025-06-21 00:37:54 +0700

    Update sponsors.controller.ts

--- backend/app/src/module/sponsors/controller/sponsors.controller.ts
+++ backend/app/src/module/sponsors/controller/sponsors.controller.ts
@@ -9,11 +9,11 @@ import {
   Req,
   UseInterceptors,
 } from '@nestjs/common';
-import { SponsorsService } from './sponsors.service';
-import { CreateSponsorDto } from './dto/create-sponsor.dto';
-import { UpdateSponsorDto } from './dto/update-sponsor.dto';
 import { MultipartInterceptor } from 'src/pkg/interceptors/multipart.interceptor';
 import { FastifyRequest } from 'fastify';
+import { CreateSponsorDto } from '../dto/sponsers/create-sponsor.dto';
+import { UpdateSponsorDto } from '../dto/sponsers/update-sponsor.dto';
+import { SponsorsService } from '../service/sponsors.service';
 
 @Controller('sponsors')
 export class SponsorsController {
