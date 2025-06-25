527fb9f455839adfeb6590afd27e701049d01bdd
Wantana Suwannapho <6631503037@lamduan.mfu.ac.th>
2025-06-25 10:33:48 +0700

    feat: add size limit for image input component

--- frontend/admin/components/ui/imageInput.tsx
+++ frontend/admin/components/ui/imageInput.tsx
@@ -1,4 +1,4 @@
-import { Button, Input } from '@heroui/react';
+import { addToast, Button, Input } from '@heroui/react';
 import { Upload, X } from 'lucide-react';
 import { useRef, useState } from 'react';
 
@@ -10,6 +10,7 @@ export type ImageInputProps = {
   onDiscard?: boolean;
   aspectRatio?: string;
   fileAccept?: string;
+  sizeLimit?: number;
 };
 
 export default function ImageInput({
@@ -20,12 +21,19 @@ export default function ImageInput({
   onDiscard,
   aspectRatio = 'aspect-square',
   fileAccept = 'image/*',
+  sizeLimit = 500 * 1024, // in byte, 1024 byte = 1 KB, 1024 * 1024 byte = 1 MB
 }: ImageInputProps) {
   const imageRef = useRef<HTMLInputElement | null>(null);
   const [previewImage, setPreviewImage] = useState<string>('');
-  const imageSrc = previewImage || (image && `${process.env.NEXT_PUBLIC_API_URL}/uploads/${image}`);
-  const isVideo = imageSrc?.endsWith('.mp4') || imageSrc?.startsWith('data:video/mp4');
-
+  const imageSrc =
+    previewImage ||
+    (image && `${process.env.NEXT_PUBLIC_API_URL}/uploads/${image}`);
+  const isVideo =
+    imageSrc?.endsWith('.mp4') || imageSrc?.startsWith('data:video/mp4');
+  const limit =
+    sizeLimit >= 1024 * 1024
+      ? `${sizeLimit / (1024 * 1024)} MB`
+      : `${sizeLimit / 1024} KB`;
   if (onDiscard && previewImage) {
     setPreviewImage('');
   }
@@ -61,7 +69,11 @@ export default function ImageInput({
       >
         {imageSrc ? (
           isVideo ? (
-            <video src={imageSrc} className="w-full h-full object-contain" controls />
+            <video
+              src={imageSrc}
+              className="w-full h-full object-contain"
+              controls
+            />
           ) : (
             <img src={imageSrc} className="w-full h-full object-contain" />
           )
@@ -70,6 +82,9 @@ export default function ImageInput({
             <Upload className="w-6 h-6 mb-2 text-default-500" />
             <p className="text-default-500 text-md font-medium">
               Upload new image
+            </p>
+            <p className="text-primary-400 text-sm font-medium">
+              {limit} Limit
             </p>
           </div>
         )}
@@ -82,6 +97,17 @@ export default function ImageInput({
         onChange={(e) => {
           const file = e.target.files?.[0];
           if (!file) return;
+
+          if (file.size > sizeLimit) {
+            addToast({
+              title: 'File size exceeds limit',
+              description: `Please upload a file smaller than ${limit}.`,
+              color: 'danger',
+              variant: 'solid',
+            });
+            e.target.value = '';
+            return;
+          }
 
           onChange(file);
 
