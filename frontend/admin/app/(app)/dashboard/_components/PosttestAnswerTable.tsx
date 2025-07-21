e4ebff3ec8de97b1b057ea8c67fb93bb3b7cc77b
MABIuS <klavivach1@@gmail.com>
2025-07-22 00:56:40 +0700

    feat: add posttest dashboard components and functionality, including data fetching and display

---
+++ frontend/admin/app/(app)/dashboard/_components/PosttestAnswerTable.tsx
@@ -0,0 +1,70 @@
+import { PottestAnswer } from "@/types/posttestAnswer";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableColumn,
+    TableHeader,
+    TableRow,
+} from "@heroui/react";
+
+type PosttestAnswerTableProp = {
+    PosttestAnswers: PottestAnswer[];
+};
+
+export default function PosttestAnswerTable({ PosttestAnswers }: PosttestAnswerTableProp) {
+    const questions = PosttestAnswers.length > 0 ? PosttestAnswers[0].answers.map(a => a.posttest) : [];
+
+    return (
+        <>
+            <Table aria-label="Posttest Answer Table">
+                <TableHeader>
+                    <>
+                        <TableColumn className="bg-transparent">Student ID</TableColumn>
+                        <TableColumn className="bg-transparent">Name</TableColumn>
+                        <TableColumn className="bg-transparent">School</TableColumn>
+                        <TableColumn className="bg-transparent">Major</TableColumn>
+                        {questions.map((q) => (
+                            <TableColumn key={q._id} className="break-words whitespace-normal max-w-[200px] bg-transparent">
+                                {q.question?.en || q.question?.th || 'Question'}
+                            </TableColumn>
+                        ))}
+                    </>
+                </TableHeader>
+
+                <TableBody emptyContent="No answers found." items={PosttestAnswers}>
+                    {(answer) => (
+                        <TableRow key={answer._id} className="border-b-1 border-t-1 border-gay-400">
+                            {[
+                                <TableCell key="username">{answer.user?.username || '-'}</TableCell>,
+                                <TableCell key="name">
+                                    {answer.user?.name?.first && answer.user?.name?.last ? `${answer.user.name.first} ${answer.user.name.last}` : '-'}
+                                </TableCell>,
+                                <TableCell key="school">
+                                    {answer.user?.metadata?.major?.school &&
+                                        typeof answer.user.metadata.major.school === 'object' &&
+                                        (answer.user.metadata.major.school.name?.en ||
+                                            answer.user.metadata.major.school.name?.th) || '-'}
+                                </TableCell>,
+
+                                <TableCell key="major">
+                                    {answer.user?.metadata?.major?.name?.en ??
+                                        answer.user?.metadata?.major?.name?.th ??
+                                        '-'}
+                                </TableCell>,
+                                ...questions.map((q) => {
+                                    const ans = answer.answers.find(a => a.posttest._id === q._id);
+                                    return (
+                                        <TableCell key={q._id}>
+                                            {ans ? ans.answer : '-'}
+                                        </TableCell>
+                                    );
+                                }),
+                            ]}
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </>
+    );
+} 
